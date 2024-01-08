243fe1425362d6cf8f640dc5134d1c120b133a06
Edwin <147519340+Skopezz@users.noreply.github.com>
2024-01-08 19:37:39 +0000

    Add randomWord function to select a word from wordList

---
+++ assets/javascript/script.js
@@ -0,0 +1,4 @@
+function randomWord() {
+let ranObj = wordList[Math.floor(Math.random() * wordList.length)];
+console.log(ranObj);
+} // Function to log a randomly selected word object from the wordList to the console.
