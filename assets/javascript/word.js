1786dc43012cb306a33e486f270c09d1ed66b930
Edwin <147519340+Skopezz@users.noreply.github.com>
2024-01-08 19:28:14 +0000

    Add script js file to javascript folder  and add word list to word js file

---
+++ assets/javascript/word.js
@@ -0,0 +1,310 @@
+let wordList = [
+    {
+        word: "constellation",
+        hint: "a group of stars forming a recognizable pattern"
+    },
+    {
+        word: "recipe",
+        hint: "instructions for preparing a particular dish"
+    },
+    {
+        word: "gravity",
+        hint: "force that attracts two objects toward each other"
+    },
+    {
+        word: "ballet",
+        hint: "a classical dance form demanding grace and precision"
+    },
+    {
+        word: "algorithm",
+        hint: "a step-by-step procedure for solving a problem"
+    },
+    {
+        word: "philosophy",
+        hint: "the study of fundamental nature of knowledge, reality, and existence"
+    },
+    {
+        word: "nebula",
+        hint: "a cloud of gas and dust in outer space"
+    },
+    {
+        word: "symphony",
+        hint: "an elaborate musical composition for full orchestra"
+    },
+    {
+        word: "genome",
+        hint: "the complete set of genes or genetic material present in a cell or organism"
+    },
+    {
+        word: "surrealism",
+        hint: "an artistic movement that seeks to express the workings of the subconscious mind"
+    },
+    {
+        word: "puzzle",
+        hint: "a game or toy designed to test ingenuity or knowledge"
+    },
+    {
+        word: "biochemistry",
+        hint: "the branch of science that explores the chemical processes within and related to living organisms"
+    },
+    {
+        word: "sociology",
+        hint: "the study of society, patterns of social relationships, social interaction, and culture"
+    },
+    {
+        word: "tundra",
+        hint: "a biome where the tree growth is hindered by low temperatures and short growing seasons"
+    },
+    {
+        word: "karate",
+        hint: "a martial art developed in the Ryukyu Islands"
+    },
+    {
+        word: "renaissance",
+        hint: "the revival of art and literature under the influence of classical models"
+    },
+    {
+        word: "psychedelic",
+        hint: "hallucinogenic drugs producing effects on perception, mood, and various cognitive processes"
+    },
+    {
+        word: "moon",
+        hint: "natural satellite of Earth"
+    },
+    {
+        word: "cryptography",
+        hint: "the practice and study of techniques for secure communication"
+    },
+    {
+        word: "magnolia",
+        hint: "a flowering plant with large, showy flowers"
+    },
+    {
+        word: "nanotechnology",
+        hint: "the manipulation of matter on an atomic, molecular, and supramolecular scale"
+    },
+    {
+        word: "origami",
+        hint: "the traditional Japanese art of paper folding"
+    },
+    {
+        word: "architecture",
+        hint: "the art and science of designing and constructing buildings"
+    },
+    {
+        word: "eclipse",
+        hint: "an astronomical event where one celestial body moves into the shadow of another"
+    },
+    {
+        word: "culinary",
+        hint: "related to cooking or the kitchen"
+    },
+    {
+        word: "metropolis",
+        hint: "a very large and densely populated industrial and commercial city"
+    },
+    {
+        word: "sonnet",
+        hint: "a poem of fourteen lines using any of a number of formal rhyme schemes"
+    },
+
+    {
+        word: "aquarium",
+        hint: "a transparent tank of water in which fish and other water creatures and plants are kept"
+    },
+
+    {
+        word: "cobra",
+        hint: "a venomous snake"
+    },
+    {
+        word: "violin",
+        hint: "a stringed musical instrument"
+    },
+    {
+        word: "goal",
+        hint: "the objective or target"
+    },
+    {
+        word: "mars",
+        hint: "red planet in our solar system"
+    },
+    {
+        word: "amazon",
+        hint: "online retail giant"
+    },
+    {
+        word: "rust",
+        hint: "programming language"
+    },
+    {
+        word: "programming",
+        hint: "related to coding"
+    },
+    {
+        word: "universe",
+        hint: "cosmic space"
+    },
+    {
+        word: "issues",
+        hint: "related to problems"
+    },
+    {
+        word: "epic",
+        hint: "legendary narrative"
+    },
+    {
+        word: "jpeg",
+        hint: "a compressed image format"
+    },
+    {
+        word: "cognitive",
+        hint: "related to mental processes"
+    },
+    {
+        word: "chart",
+        hint: "visual representation of data"
+    },
+    {
+        word: "oasis",
+        hint: "an isolated fertile area in a desert"
+    },
+    {
+        word: "soccer",
+        hint: "popular outdoor game"
+    },
+    {
+        word: "boardgame",
+        hint: "related to indoor games"
+    },
+    {
+        word: "whatsapp",
+        hint: "popular messaging app"
+    },
+    {
+        word: "gitlab",
+        hint: "web-based Git repository manager"
+    },
+    {
+        word: "bmp",
+        hint: "a bitmap image file format"
+    },
+    {
+        word: "platinum",
+        hint: "precious silver-white metal"
+    },
+    {
+        word: "smartphone",
+        hint: "an advanced mobile device"
+    },
+    {
+        word: "cpu",
+        hint: "central processing unit"
+    },
+    {
+        word: "ruby",
+        hint: "programming language"
+    },
+    {
+        word: "bing",
+        hint: "popular search engine"
+    },
+    {
+        word: "amsterdam",
+        hint: "capital city of the Netherlands"
+    },
+    {
+        word: "excel",
+        hint: "spreadsheet software by Microsoft"
+    },
+    {
+        word: "postgresql",
+        hint: "an open-source relational database system"
+    },
+    {
+        word: "bhutan",
+        hint: "small landlocked country in Asia"
+    },
+    {
+        word: "saxophone",
+        hint: "a brass musical instrument"
+    },
+    {
+        word: "blockchain",
+        hint: "technology behind cryptocurrencies"
+    },
+    {
+        word: "volt",
+        hint: "unit of electrical potential difference"
+    },
+    {
+        word: "jupiter",
+        hint: "largest planet in our solar system"
+    },
+    {
+        word: "firewall",
+        hint: "security device for network protection"
+    },
+    {
+        word: "message",
+        hint: "communication in written or spoken words"
+    },
+    {
+        word: "css",
+        hint: "style sheet language for the web"
+    },
+    {
+        word: "oxygen",
+        hint: "element essential for life"
+    },
+    {
+        word: "concept",
+        hint: "an abstract idea or general notion"
+    },
+    {
+        word: "mainframe",
+        hint: "large and powerful computer"
+    },
+    {
+        word: "pdf",
+        hint: "portable document format"
+    },
+    {
+        word: "png",
+        hint: "a raster image file format"
+    },
+    {
+        word: "explore",
+        hint: "to travel in or through an unfamiliar area"
+    },
+    {
+        word: "lock",
+        hint: "device for securing something"
+    },
+    {
+        word: "morocco",
+        hint: "country in North Africa"
+    },
+    {
+        word: "psycho",
+        hint: "classic psychological thriller film"
+    },
+    {
+        word: "singapore",
+        hint: "modern city-state in Southeast Asia"
+    },
+    {
+        word: "snapshot",
+        hint: "a photograph taken quickly"
+    },
+    {
+        word: "amazon",
+        hint: "the longest river in South America"
+    },
+    {
+        word: "shower",
+        hint: "fall of rain in drops"
+    },
+    
+
+];
