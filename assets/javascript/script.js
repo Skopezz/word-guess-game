1aa359f6b718274317b1ea35084997263f0f3756
Edwin <147519340+Skopezz@users.noreply.github.com>
2024-01-09 12:51:29 +0000

    Display word hint and set up disabled input fields

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,5 +1,6 @@
 const inputs = document.querySelector(".inputs"), // Selects the HTML element that will display the guessed letters or underscores
-resetBtn = document.querySelector(".reset-btn"); // Selects the reset button element in the HTML using the class "reset-btn"
+resetBtn = document.querySelector(".reset-btn"), // Selects the reset button element in the HTML using the class "reset-btn"
+hint = document.querySelector(".hint span");// Selecting the hint span element for displaying hints in the word guessing game
 
 // Function to log a randomly selected word object from the wordList to the console.
 function randomWord() {
@@ -7,13 +8,13 @@ let ranObj = wordList[Math.floor(Math.random() * wordList.length)]; // Generate 
 let word = ranObj.word; // Retrieves the randomly selected word from the wordList object
 console.log(ranObj); 
 
+hint.innerText = ranObj.hint; // Setting the hint text content to the hint associated with the randomly selected word
+
 let html = "";
 for (let i = 0; i < word.length; i++) {
-    html += `<input type="text" disabled>`;
-    inputs.innerHTML = html; 
-    // Generate and display input fields for each character in the word on the webpage
+    html += `<input type="text" disabled>`; // Creating a disabled text input for each character of the word
 }
-
+inputs.innerHTML = html; // Update the content of the "inputs" element to display disabled text input elements
 } 
 
 randomWord();
