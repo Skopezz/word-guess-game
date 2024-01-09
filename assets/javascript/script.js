9c4795cf72ac49ce888dd9d9066b94dfdfa2fe71
Edwin <147519340+Skopezz@users.noreply.github.com>
2024-01-09 16:39:22 +0000

    Add text transform to uppercase for letters input, update display on correct guess and provide feedback for invalid input

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -4,10 +4,12 @@ hint = document.querySelector(".hint span"), // Selecting the hint span element 
 typingInput = document.querySelector(".typing-input"); // Selecting the input field for typing guesses in the word guessing game
 
 
+let word;
+
 // Function to log a randomly selected word object from the wordList to the console.
 function randomWord() {
 let ranObj = wordList[Math.floor(Math.random() * wordList.length)]; // Generate a random index to select a word from the wordList
-let word = ranObj.word; // Retrieves the randomly selected word from the wordList object
+word = ranObj.word; // Retrieves the randomly selected word from the wordList object
 console.log(word); 
 
 hint.innerText = ranObj.hint; // Setting the hint text content to the hint associated with the randomly selected word
@@ -24,11 +26,21 @@ randomWord();
 // Function to handle user input events and obtain the lowercase value for further gameplay.
 function initGame(e) {
     let key = e.target.value.toLowerCase();
-    // Check if the input key is a letter (A-Z or a-z) using a regular expression.
+    // Checks if the input key is a letter (A-Z or a-z) using a regular expression.
     if(key.match(/^[A-Za-z]+$/)) {
+        console.log(key);
+        if(word.includes(key)) // Checks if the current guessed letter is included in the target word
+        for (let i = 0; i < word.length; i++) {
+            // Checks if the current letter in the target word matches the guessed letter
+            if(word[i] == key) {
+                inputs.querySelectorAll("input")[i].value = key;
+            }
+    } else {  
+        console.log("letter not found")
 
     }
-    console.log(key);
+    }
+    
 }
 
 resetBtn.addEventListener("click", randomWord); // Adds a click event listener to the reset button, triggering the randomWord function to generate a new word for the game when clicked
